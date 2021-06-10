970016348c22544e4541dd5a8ec87d682d15dea1
LucasJappert <lucasnicolasjappert@gmail.com>
2021-06-10 14:49:31 -0300

    prod queries

--- queries.js
+++ queries.js
@@ -1,18 +1,18 @@
 const Pool = require('pg').Pool
-// const pool = new Pool({
-//   user: 'nxdlimaoofkkmu',
-//   host: 'ec2-52-6-77-239.compute-1.amazonaws.com',
-//   database: 'dam6liqvdcltn5',
-//   password: 'f417629cf3a820f23100dc37e5d227be51c90ca56730bcd684c49afa86b66350',
-//   port: 5432,
-// })
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'Base1',
-    password: 'luks2018',
-    port: 5432,
+  user: 'nxdlimaoofkkmu',
+  host: 'ec2-52-6-77-239.compute-1.amazonaws.com',
+  database: 'dam6liqvdcltn5',
+  password: 'f417629cf3a820f23100dc37e5d227be51c90ca56730bcd684c49afa86b66350',
+  port: 5432,
 })
+// const pool = new Pool({
+//     user: 'postgres',
+//     host: 'localhost',
+//     database: 'Base1',
+//     password: 'luks2018',
+//     port: 5432,
+// })
 
 const getUsers = (request, response) => {
     pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
