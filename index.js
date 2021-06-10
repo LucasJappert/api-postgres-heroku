7e460fea7dab73091b7ca97ab3fcf79c4cdf5e18
LucasJappert <lucasnicolasjappert@gmail.com>
2021-06-10 15:50:22 -0300

    fix 1

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 5000;
 // app.use();
 app.get('/', (request, response) => {
     //response.json({ info: 'Node.js, Express, and Postgres API WORKING!' })
-    request.send("WORKING!");
+    response.send("WORKING!");
 });
 
 app.get('/users', db.getUsers);
